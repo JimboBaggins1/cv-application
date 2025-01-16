a34687ba925a5af4d977867c2eefe70a8ec03911
James Anderson <james.anderson98@outlook.com>
2025-01-16 14:21:35 +0000

    Styling tweaks and start cleaning up

--- src/Components.jsx
+++ src/Components.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState } from "react";
+import "./Component.css";
 
 // const CustomInput = () => {
 //     const [value, setValue] = useState("");
@@ -8,7 +9,7 @@ import { useState } from 'react';
 //     };
 
 //     return (
-//         <input 
+//         <input
 //             type="text"
 //             value={value}
 //             onChange={handleChange}
@@ -39,53 +40,84 @@ import { useState } from 'react';
 //     )
 // }
 
-const Button = ({ text, fontSize = 12, handleClick }) => {
-    const buttonStyle = {
-        fontSize: fontSize + 'px'
-    };
+const Button = ({ text, handleClick }) => {
+  return (
+    <button
+      onClick={handleClick}
+      className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+    >
+      {text}
+    </button>
+  );
+};
 
-    return (
-        <button onClick={handleClick} style={buttonStyle}>
-            {text}
-        </button>
-    );
-}
+export const GeneralInformationForm = () => {
+  const [editMode, setEditMode] = useState(false);
 
-export const Form = () => {
-    const [editMode, setEditMode] = useState(false);
+  const [generalFormData, setGeneralFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    address: "",
+  });
 
-    const [generalFormData, setGeneralFormData] = useState({
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        address: ''
-    });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-    const handleChange = e => {
-        const { name, value } = e.target;
+    setGeneralFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-        setGeneralFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
+  const handleButtonClick = (editMode) => {
+    editMode === false ? setEditMode(true) : setEditMode(false);
+  };
 
-    const handleButtonClick = (editMode) => {
-        editMode === false ? setEditMode(true) : setEditMode(false);
-    };
+  return (
+    <>
+      <form className="w-full max-w-xl p-0.5">
+        {Object.keys(generalFormData).map((prop) => {
+          return (
+            <>
+              <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/3">
+                  <label
+                    htmlFor="{prop}"
+                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  >
+                    {prop}:
+                  </label>
+                </div>
+                <div className="md:w-2/3">
+                  <input
+                    id={prop}
+                    key={prop}
+                    type={
+                      prop === "email"
+                        ? "email"
+                        : prop === "phone"
+                        ? "tel"
+                        : "text"
+                    }
+                    name={prop}
+                    value={generalFormData[prop]}
+                    onChange={handleChange}
+                    disabled={!editMode}
+                    className= {(editMode ? "bg-slate-50" : "bg-gray-200") + " appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"}
+                  />
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </form>
 
-    return (
-        <>
-            <form style={{ display: 'flex', flexDirection: 'column', maxWidth: '600px', margin: 'auto' }}>
-                {Object.keys(generalFormData).map(prop => {
-                    return editMode === false 
-                                    ? <label>{prop}<span>{generalFormData[prop]}</span></label> 
-                                    : <label>{prop}<input key={prop} type={prop === 'email' ? 'email': prop === 'phone' ? 'tel' : 'text'} name={prop} value={generalFormData[prop]} onChange={handleChange} /></label>
-                })}
-            </form>
-            
-            <Button text={editMode ? 'View' : 'Edit'} handleClick={() => handleButtonClick(editMode)}/>
-        </>
-    )
-}
+      <Button
+        text={editMode ? "View" : "Edit"}
+        handleClick={() => handleButtonClick(editMode)}
+      />
+    </>
+  );
+};
