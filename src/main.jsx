b48b539322e121a0e98b7c2761c2f6db75f50527
James Anderson <james.anderson98@outlook.com>
2025-01-17 15:20:05 +0000

    Run App component from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Form } from './Components.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Form />
+    <App />
   </StrictMode>,
 )
