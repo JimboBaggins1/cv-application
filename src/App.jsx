93fc192847ab0aac132a196079d31ccda5f1bd3e
James Anderson <james.anderson98@outlook.com>
2025-01-17 15:18:17 +0000

    Move components into components folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
-import { Collapsible } from './Collapsible.jsx';
-import { GeneralInformationForm } from './Components.jsx';
+import { Collapsible } from './components/Collapsible.jsx';
+import { GeneralInformationForm } from './components/Components.jsx';
 
 function App() {
   return (
